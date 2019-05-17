d532e7a479cbbdcaad54383ed4a1fdf11cc6f8c4
cornie <cornie.wiens@vidir.com>
2019-05-17 15:35:16 -0500

    Added call to CustomVision API with selected image
    The selected image is now sent to the CustomVision API and we get a response back.

--- Wpf-Landmark-AI/Wpf-Landmark-AI/MainWindow.xaml.cs
+++ Wpf-Landmark-AI/Wpf-Landmark-AI/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,9 +33,46 @@ namespace Wpf_Landmark_AI
         {
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = "Image Files (*.png; *jgp)|*.png;*jpg;*jpeg|All Files (*.*)|*.*";
+
             if (dialog.ShowDialog().Value)
             {
                 string fileName = dialog.FileName;
+
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePreditionAsync(fileName);
+            }
+        }
+
+        private async void MakePreditionAsync(string fileName)
+        {
+            #region Set URL and Keys
+            string url = "";
+            string predictionKey = "";
+            string contentType = "application/octet-stream";
+
+            try
+            {
+                url = File.ReadAllText(@"C:\Users\cornie\Documents\Microsoft-CustomVision-AI\Image-URL.txt");
+                predictionKey = File.ReadAllText(@"C:\Users\cornie\Documents\Microsoft-CustomVision-AI\Prediction-Key.txt");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            } 
+            #endregion
+
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+                    var response = await client.PostAsync(url, content);
+                }
             }
         }
     }
